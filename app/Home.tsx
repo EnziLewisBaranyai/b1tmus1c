ac3652bf21e3bc8bd43bc0c8158f644016e065f6
Enzi Lewis-Baranyai <enzi_lewis@icloud.com>
2023-10-27 09:13:43 -0400

    login and signup

--- app/Home.tsx
+++ app/Home.tsx
@@ -2,7 +2,8 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "./custom.css";
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import Link from "next/link";
 
 function Home() {
   return (
@@ -18,7 +19,10 @@ function Home() {
           <div className="col-md-6 text-center">
             <h1 className="display-4">Welcome to B1tMus1c</h1>
             <p className="lead">Share your favorite music with the world!</p>
-            <Link to="/" className="btn btn-outline-light btn-lg mt-3">
+            <Link
+              href="/protected"
+              className="btn btn-outline-light btn-lg mt-3"
+            >
               Get Started
             </Link>
           </div>
