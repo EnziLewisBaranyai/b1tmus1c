d0ecb7f486974a5c0af0da60b56d28edf5c98d10
Nick-Reichlin <nickreichlin@gmail.com>
2023-10-27 14:24:43 -0400

    Small changes

--- app/Home.tsx
+++ app/Home.tsx
@@ -3,10 +3,18 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./custom.css";
 import React from "react";
 import { Link } from "react-router-dom";
+import img from '/public/BackSplash.jpg';
 
 function Home() {
+  const backgroundImageStyle = {
+    backgroundImage: `url("${img}")`,
+    backgroundSize: "contain",
+    backgroundPosition: "center center",
+    width: "100%",
+    height: "100vh",
+  };
   return (
-    <div className="background">
+    <div style={backgroundImageStyle}>
       <div
         className="container-fluid bg-black text-orange"
         style={{ height: "100vh" }}
